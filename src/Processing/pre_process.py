7795c25ea5583cf3926e7e48926e658a37ad002c
storrijos <sergio.torrijos@estudiante.uam.es>
2019-11-19 21:26:33 +0100

    Pre process commmand added

--- src/Processing/pre_process.py
+++ src/Processing/pre_process.py
@@ -1,5 +1,22 @@
 import pandas as pd
+import click
 import os
+
+class LineDataset:
+
+    def __init__(self, user_id, timestamp, lat, longitude, item_id):
+        self.user_id = user_id
+        self.timestamp = timestamp
+        self.lat = lat
+        self.long = longitude
+        self.item_id = item_id
+
+    def __str__(self):
+        return self.user_id + '    ' \
+               + self.item_id + '    ' \
+               + self.lat + '    ' \
+               + self.long + '    ' \
+               + self.timestamp
 
 """
 def find_path():
@@ -75,45 +92,6 @@ class ProcessData:
         dataset = dataset.drop(columns=['item_id'])
         return dataset
 
-    def printToFile(self, file, salida):
-        with open(file, 'w') as f:
-            for item in salida:
-                f.write("%s\n" % item)
-
-    def readPOISandCoordinates(self, txt_file):
-        token = open(txt_file, "r")
-        linestoken = token.readlines()
-        poi_map = {}
-        for line in linestoken:
-            poi_id = line.split()[0]
-            lat = line.split()[1]
-            longitude = line.split()[2]
-            poi_map[poi_id] = [lat, longitude]
-        return poi_map
-
-    def readFileGroupItem(self, txt_file, pois):
-        token = open(txt_file, "r")
-        linestoken = token.readlines()
-        items_lines = []
-        count = 1
-        for line in linestoken:
-            percentage = (float(count) / float(len(linestoken))) * 100
-            print('Setting up items' + "%.2f" % percentage + '%')
-            count += 1
-            item_id = str(line.split()[1])
-            user_id = line.split()[0]
-            timestamp = line.split()[3]
-            poi_lat_long = pois[item_id]
-            poi_lat = poi_lat_long[0]
-            poi_long = poi_lat_long[1]
-            line_dataset = LineDataset(user_id, timestamp, poi_lat, poi_long, item_id)
-            items_lines.append(line_dataset)
-        return items_lines
-
-    def save_dataset_with_coords(self):
-        pois_coords = self.readPOISandCoordinates('entradas/DatosNewYork/POIS_Coords_Foursquare.txt')
-        salida = self.readFileGroupItem('entradas/DatosNewYork/US_NewYorkTempTrain.txt', pois_coords)
-        self.printToFile('salidas/US_NewYork_POIS_Coords', salida)
 
     def loadData(filename):
         data = pd.read_csv(filename, delim_whitespace=True, header=None)
@@ -121,18 +99,50 @@ class ProcessData:
         summary_stats = data.describe()
         return data
 
-class LineDataset:
+def printToFile(file, salida):
+    with open(file, 'w') as f:
+        for item in salida:
+            f.write("%s\n" % item)
 
-    def __init__(self, user_id, timestamp, lat, longitude, item_id):
-        self.user_id = user_id
-        self.timestamp = timestamp
-        self.lat = lat
-        self.long = longitude
-        self.item_id = item_id
+def readPOISandCoordinates(txt_file):
+    token = open(txt_file, "r")
+    linestoken = token.readlines()
+    poi_map = {}
+    for line in linestoken:
+        poi_id = line.split()[0]
+        lat = line.split()[1]
+        longitude = line.split()[2]
+        poi_map[poi_id] = [lat, longitude]
+    return poi_map
 
-    def __str__(self):
-        return self.user_id + '    ' \
-               + self.item_id + '    ' \
-               + self.lat + '    ' \
-               + self.long + '    ' \
-               + self.timestamp
+def readFileGroupItem(txt_file, pois):
+    token = open(txt_file, "r")
+    linestoken = token.readlines()
+    items_lines = []
+    count = 1
+    for line in linestoken:
+        percentage = (float(count) / float(len(linestoken))) * 100
+        print('Setting up items' + "%.2f" % percentage + '%')
+        count += 1
+        item_id = str(line.split()[1])
+        user_id = line.split()[0]
+        timestamp = line.split()[3]
+        poi_lat_long = pois[item_id]
+        poi_lat = poi_lat_long[0]
+        poi_long = poi_lat_long[1]
+        line_dataset = LineDataset(user_id, timestamp, poi_lat, poi_long, item_id)
+        items_lines.append(line_dataset)
+    return items_lines
+
+@click.command()
+@click.option('--input_file', default='US_NewYorkTempTrain.txt', help='Dataset.')
+@click.option('--coords_file', default='POIS_Coords_Foursquaretxt', help='Dataset.')
+@click.option('--output_file', default='US_NewYorkTempTrain_short.txt', help='Dataset.')
+def save_dataset_with_coords(input_file, coords_file, output_file):
+    pois_coords = readPOISandCoordinates(coords_file)
+    salida = readFileGroupItem(input_file, pois_coords)
+    printToFile('entradas/' + str(output_file), salida)
+
+if __name__ == '__main__':
+    save_dataset_with_coords()
+
